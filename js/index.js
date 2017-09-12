898a314189a948ecf6a086e708ff4a8570853904
独孤人 <1143910315@qq.com>
2017-09-12 18:18:52 +0800

    增加天气

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
 setTimeout(function () {
 	process.style.width="20%";
 },2000);
+var shichenshuju={
+	"子时":{"start":125,"stop":130},
+	"丑时":{"start":130,"stop":135},
+	"寅时":{"start":135,"stop":145},
+	"卯时":{"start":145,"stop":170},
+	"辰时":{"start":170,"stop":210},
+	"巳时":{"start":210,"stop":235},
+	"午时":{"start":235,"stop":240},
+	"未时":{"start":0,"stop":15},
+	"申时":{"start":15,"stop":60},
+	"酉时":{"start":60,"stop":95},
+	"戌时":{"start":95,"stop":115},
+	"亥时":{"start":115,"stop":125}
+}
