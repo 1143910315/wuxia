c566ac23d1af39d6617c15cdee05c3f786cae22f
独孤人 <1143910315@qq.com>
2017-09-13 17:57:00 +0800

    Revert "备份"
    This reverts commit f2c4528c33ac4cf3b6b116a416c08d5ae28c2384.

--- js/index.js
+++ js/index.js
@@ -1,26 +1,18 @@
 var shichenshuju={
-	"子时":{"start":125,"stop":130,"id":"zi"},
-	"丑时":{"start":130,"stop":135,"id":"chou"},
-	"寅时":{"start":135,"stop":145,"id":"yin"},
-	"卯时":{"start":145,"stop":170,"id":"mao"},
-	"辰时":{"start":170,"stop":210,"id":"chen"},
-	"巳时":{"start":210,"stop":235,"id":"si"},
-	"午时":{"start":235,"stop":240,"id":"wu"},
-	"未时":{"start":0,"stop":15,"id":"wei"},
-	"申时":{"start":15,"stop":60,"id":"shen"},
-	"酉时":{"start":60,"stop":95,"id":"you"},
-	"戌时":{"start":95,"stop":115,"id":"xu"},
-	"亥时":{"start":115,"stop":125,"id":"hai"}
+	"子时":{"start":125,"stop":130},
+	"丑时":{"start":130,"stop":135},
+	"寅时":{"start":135,"stop":145},
+	"卯时":{"start":145,"stop":170},
+	"辰时":{"start":170,"stop":210},
+	"巳时":{"start":210,"stop":235},
+	"午时":{"start":235,"stop":240},
+	"未时":{"start":0,"stop":15},
+	"申时":{"start":15,"stop":60},
+	"酉时":{"start":60,"stop":95},
+	"戌时":{"start":95,"stop":115},
+	"亥时":{"start":115,"stop":125}
 };
-var handle=window.addEventListener("load",function () {
-	var width=window.screen.availWidth;
-	var min=parseInt(width/240);
-	var max=min*240;
-	var space=(width-max)/2;
-	document.styleSheets[0].rules[5].style.left=space+"px";
-	
-});
-setInterval(function() {
+setTimeout(function() {
 	var date=new Date();
 	var fen=date.getHours()%4*60+date.getMinutes();
 	for (var i in shichenshuju) {
