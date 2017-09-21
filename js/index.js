d16d08dfd4357877614a7602e8a4c93e322999bc
独孤人 <1143910315@qq.com>
2017-09-21 15:22:03 +0800

    修改结构

--- js/index.js
+++ js/index.js
@@ -1,31 +1,29 @@
-var shichenshuju={
-	"子时":{"start":125,"stop":130,"id":"zi"},
-	"丑时":{"start":130,"stop":135,"id":"chou"},
-	"寅时":{"start":135,"stop":145,"id":"yin"},
-	"卯时":{"start":145,"stop":170,"id":"mao"},
-	"辰时":{"start":170,"stop":210,"id":"chen"},
-	"巳时":{"start":210,"stop":235,"id":"si"},
-	"午时":{"start":235,"stop":240,"id":"wu"},
-	"未时":{"start":0,"stop":15,"id":"wei"},
-	"申时":{"start":15,"stop":60,"id":"shen"},
-	"酉时":{"start":60,"stop":95,"id":"you"},
-	"戌时":{"start":95,"stop":115,"id":"xu"},
-	"亥时":{"start":115,"stop":125,"id":"hai"}
-};
-var handle=window.addEventListener("load",function () {
+var shichenshuju=[{"start":125,"stop":130,"id":"zi","time":"子时"},
+	{"start":130,"stop":135,"id":"chou","time":"丑时"},
+	{"start":135,"stop":145,"id":"yin","time":"寅时"},
+	{"start":145,"stop":170,"id":"mao","time":"卯时"},
+	{"start":170,"stop":210,"id":"chen","time":"辰时"},
+	{"start":210,"stop":235,"id":"si","time":"巳时"},
+	{"start":235,"stop":240,"id":"wu","time":"午时"},
+	{"start":0,"stop":15,"id":"wei","time":"未时"},
+	{"start":15,"stop":60,"id":"shen","time":"申时"},
+	{"start":60,"stop":95,"id":"you","time":"酉时"},
+	{"start":95,"stop":115,"id":"xu","time":"戌时"},
+	{"start":115,"stop":125,"id":"hai","time":"亥时"}
+];
+var initial=function () {
 	var width=window.screen.availWidth;
 	var min=parseInt(width/240);
 	var max=min*240;
 	var space=(width-max)/2;
 	document.styleSheets[0].rules[5].style.left=space+"px";
-	
-});
+};
 setInterval(function() {
 	var date=new Date();
 	var fen=date.getHours()%4*60+date.getMinutes();
 	for (var i in shichenshuju) {
 		if (fen>=shichenshuju[i].start&&fen<=shichenshuju[i].stop) {
-			shichen.innerHTML=i+"<br \>剩余"+(shichenshuju[i].stop-fen)+"分钟<br \>已过"+(fen-shichenshuju[i].start)+"分钟<br \>总共"+(shichenshuju[i].stop-shichenshuju[i].start)+"分钟";
+			shichen.innerHTML=shichenshuju[i].time+"<br \>剩余"+(shichenshuju[i].stop-fen)+"分钟<br \>已过"+(fen-shichenshuju[i].start)+"分钟<br \>总共"+(shichenshuju[i].stop-shichenshuju[i].start)+"分钟";
 		}
 	}
 }, 1000);
